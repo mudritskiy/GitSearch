b05e7443e630ed3b422bff8197d856681c18ceb5
Volodymyr Mudrytskyi <mudritskiy@gmail.com>
2019-06-04 23:32:13 +0300

    #1
    project created

---
+++ GitSearch/GitSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitSearch
+//
+//  Created by Vldmr on 6/4/19.
+//  Copyright © 2019 mudritskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
