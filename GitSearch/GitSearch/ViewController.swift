874bab9074710e1a6fb38b381a74c103e669195a
Volodymyr Mudrytskyi <mudritskiy@gmail.com>
2019-06-05 21:44:09 +0300

    #5
    new code

--- GitSearch/GitSearch/ViewController.swift
+++ GitSearch/GitSearch/ViewController.swift
@@ -13,20 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let labelSeparator = (width: Int(self.view.frame.size.width/3), height: 200)
+        let borderTop = 20
+        let viewWidth = Int(self.view.frame.size.width)
+        let labelSeparator = (width: Int(viewWidth/3), height: 100)
         
-        let headerTitle = UILabel(frame: CGRect(x: 0, y: 0, width: labelSeparator.width, height: labelSeparator.height))
+        let headerTitle = UILabel(frame: CGRect(x: 0, y: borderTop, width: labelSeparator.width, height: labelSeparator.height))
         headerTitle.text = "GIT"
         headerTitle.textAlignment = .right
         headerTitle.textColor = UIColor.white
         headerTitle.backgroundColor = UIColor.orange
+        headerTitle.font = UIFont.boldSystemFont(ofSize: 17)
         self.view.addSubview(headerTitle)
 
-        let headerTitle2 = UILabel(frame: CGRect(x: labelSeparator.width, y: 0, width: (Int(self.view.frame.size.width) - labelSeparator.width), height: labelSeparator.height))
+        let headerTitle2 = UILabel(frame: CGRect(x: labelSeparator.width, y: borderTop, width: (viewWidth - labelSeparator.width), height: labelSeparator.height))
         headerTitle2.text = "SEARCH"
         headerTitle2.textAlignment = .left
-        headerTitle2.textColor = UIColor.white
-        headerTitle2.backgroundColor = UIColor.blue
+        headerTitle2.textColor = UIColor.black
+        headerTitle2.backgroundColor = UIColor.white
+        headerTitle.font = UIFont.boldSystemFont(ofSize: 17)
         self.view.addSubview(headerTitle2)
 
     }
