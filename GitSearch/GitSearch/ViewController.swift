433697b1a0fdf056c1709003785388d788501f9c
Volodymyr Mudrytskyi <mudritskiy@gmail.com>
2019-06-05 22:26:15 +0300

    #6
    label point

--- GitSearch/GitSearch/ViewController.swift
+++ GitSearch/GitSearch/ViewController.swift
@@ -13,25 +13,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let lableFont = UIFont.boldSystemFont(ofSize: 34)
         let borderTop = 20
         let viewWidth = Int(self.view.frame.size.width)
-        let labelSeparator = (width: Int(viewWidth/3), height: 100)
+        let labelSeparator = (width: Int(viewWidth/3), height: 50)
         
-        let headerTitle = UILabel(frame: CGRect(x: 0, y: borderTop, width: labelSeparator.width, height: labelSeparator.height))
-        headerTitle.text = "GIT"
-        headerTitle.textAlignment = .right
-        headerTitle.textColor = UIColor.white
-        headerTitle.backgroundColor = UIColor.orange
-        headerTitle.font = UIFont.boldSystemFont(ofSize: 17)
-        self.view.addSubview(headerTitle)
+        let labelGit = UILabel(frame: CGRect(x: 0, y: borderTop, width: labelSeparator.width, height: labelSeparator.height))
+        labelGit.text = "GIT"
+        labelGit.textAlignment = .right
+        labelGit.textColor = UIColor.white
+        labelGit.backgroundColor = UIColor(red:0.98, green:0.56, blue:0.04, alpha:1.0) // orange
+        labelGit.font = lableFont
+        self.view.addSubview(labelGit)
 
-        let headerTitle2 = UILabel(frame: CGRect(x: labelSeparator.width, y: borderTop, width: (viewWidth - labelSeparator.width), height: labelSeparator.height))
-        headerTitle2.text = "SEARCH"
-        headerTitle2.textAlignment = .left
-        headerTitle2.textColor = UIColor.black
-        headerTitle2.backgroundColor = UIColor.white
-        headerTitle.font = UIFont.boldSystemFont(ofSize: 17)
-        self.view.addSubview(headerTitle2)
+        let labelSearch = UILabel(frame: CGRect(x: labelSeparator.width, y: borderTop, width: (viewWidth - labelSeparator.width), height: labelSeparator.height))
+        labelSearch.text = "SEARCH"
+        labelSearch.textAlignment = .left
+        labelSearch.textColor = UIColor.white
+        labelSearch.backgroundColor = UIColor(red:0.41, green:0.56, blue:0.81, alpha:1.0) // blue
+        labelSearch.font = lableFont
+        self.view.addSubview(labelSearch)
 
     }
 
