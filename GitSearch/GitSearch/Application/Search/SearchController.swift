005701f3c40703b76b29f7bf2a5a9c1812c50f03
Mudrytskyi Volodymyr <mudritskiy@gmail.com>
2019-08-03 12:19:43 +0300

    changed setting prefferedLargeTitle to largeTitleDisplayMode (viewWillAppear refused)

--- GitSearch/GitSearch/Application/Search/SearchController.swift
+++ GitSearch/GitSearch/Application/Search/SearchController.swift
@@ -103,7 +103,11 @@ class SearchController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.largeTitleDisplayMode = .never
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         view?.backgroundColor = UIColor.white
+        
         setupSubviews()
     }
     
@@ -111,10 +115,6 @@ class SearchController: UIViewController, UITextFieldDelegate {
         
         view.endEditing(true)
         super.touchesBegan(touches, with: event)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        navigationController?.navigationBar.prefersLargeTitles = false
     }
     
     func fetchRepositoriesHeader(from urlString: String, completion: @escaping (SearchInfo) -> ()) {
