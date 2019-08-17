1bedec38fe490214b189b5d8830cd9a977d534b4
Mudrytskyi Volodymyr <mudritskiy@gmail.com>
2019-08-17 23:59:11 +0300

    solved problem & moved some methods into classes they respond

--- GitSearch/Models/SearchItem.swift
+++ GitSearch/Models/SearchItem.swift
@@ -18,6 +18,47 @@ class SearchItem: Decodable {
     let language: String?
     let html_url: String?
     let description: String?
+
+    func ToArray(props: inout [String]) -> Dictionary<String, String> {
+        var result = [String: String]()
+        var childValue: String?
+        let propsFilled = props.count != 0
+        
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM dd,yyyy"
+        
+        let mirror = Mirror(reflecting: self)
+        for child in mirror.children  {
+            let childKey = child.label!
+            if childKey != "owner" {
+                if let value = child.value as? Int {
+                    childValue = String(value)
+                }
+                else if let value = child.value as? Double {
+                    childValue = String(value)
+                }
+                else if let value = child.value as? String {
+                    if let date: Date = dateFormatterGet.date(from: value) {
+                        childValue = dateFormatter.string(from: date)
+                    } else {
+                        childValue = value
+                    }
+                }
+                else {childValue = ""}
+            } else {
+                childValue = (child.value as! Owners).login!
+            }
+            if !propsFilled {
+                props.append(childKey)
+            }
+            result[childKey] = childValue
+        }
+        return result
+    }
+    
 }
 
 struct Owners: Decodable {
