1d3f0c54af471568e2772e7c695ee78aebe37a69
Mudrytskyi Volodymyr <mudritskiy@gmail.com>
2019-08-27 06:49:39 +0300

    add networking service parts - planing

---
+++ GitSearch/Networking/HTTP/HTTPNetworkRoutes.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPNetworkRoutes.swift
+//  GitSearch
+//
+//  Created by Vldmr on 8/26/19.
+//  Copyright © 2019 mudritskiy. All rights reserved.
+//
+
+import Foundation
+
+public enum HTTPNetworkRoutes: String {
+    case searchRepositories = "/search/repositories"
+}
---
+++ GitSearch/Networking/Service/PostService.swift
@@ -0,0 +1,9 @@
+//
+//  PostService.swift
+//  GitSearch
+//
+//  Created by Vldmr on 8/26/19.
+//  Copyright © 2019 mudritskiy. All rights reserved.
+//
+
+import Foundation
